0da9207ef95f528c08cae04fb8b11bf576ba76bf
TerxMexNinja <60802586+TerxMexNinja@users.noreply.github.com>
2020-02-14 20:28:55 -0600

    start interactable

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace Core
+//{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class Interactable : MonoBehaviour
+    {
+
+        private void Start()
+        { 
+
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+           // Highlight object
+           //object becomes interactable
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+//}
