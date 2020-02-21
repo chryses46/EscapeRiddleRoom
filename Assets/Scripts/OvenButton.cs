9b4fbb458a3f5d4369ff3a30e2a9cfd3f15ad151
chryses46 <danjfrank08@gmail.com>
2020-02-21 12:55:57 -0600

    kitchen puzzle complete

---
+++ Assets/Scripts/OvenButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Core.Interactables
+{
+    public class OvenButton : MonoBehaviour
+    {
+        RectTransform rectTransform;
+
+        [SerializeField] OvenButton[] connectedButtons;
+
+        private void Start()
+        {
+            rectTransform = gameObject.GetComponent<RectTransform>();
+        }
+
+        public void FlipButton(bool isExternalCall)
+        {
+            float currentButtonDegreeofRotation = rectTransform.eulerAngles.z;
+
+            if (currentButtonDegreeofRotation >= 180)
+            {
+                Vector3 currentRotation = rectTransform.eulerAngles;
+                currentRotation.z = 0;
+                rectTransform.eulerAngles = currentRotation;
+
+            }
+            else if(currentButtonDegreeofRotation <= 180)
+            {
+                Vector3 currentRotation = rectTransform.eulerAngles;
+                currentRotation.z = 180;
+                rectTransform.eulerAngles = currentRotation;
+            }
+
+            if(!isExternalCall) // meaning not being called by aonther button
+            {
+                foreach (var button in connectedButtons)
+                {
+                    button.FlipButton(true);
+                }
+            }
+        }
+
+        public bool IsSwitchUp()
+        {
+            if(rectTransform.eulerAngles.z < 1 && rectTransform.eulerAngles.z >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchPuzzle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Interactables
+{
+    public class SwitchPuzzle : MonoBehaviour
+    {
+        [SerializeField] OvenButton[] ovenButtons;
+
+        [SerializeField] GameObject[] ovenFingers;
+
+        OvenButton currentOvenButton;
+
+        int currentFinger = 0;
+
+        bool dpadActive;
+
+        private void Start()
+        {
+            SetButtonAndFinger();
+        }
+
+        private void Update()
+        {
+            if (StateMachineController.instance.gameState == StateMachineController.State.Puzzle)
+            {
+                MoveFinger();
+
+                SelectButton();
+
+                ExitPuzzle();
+            }
+        }
+
+        private void ExitPuzzle()
+        {
+            if(Input.GetButtonDown("Cancel"))
+            {
+                GameManager.instance.ExitPuzzle();
+            }
+        }
+
+        private void SelectButton()
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                currentOvenButton.FlipButton(false);
+            }
+
+            CheckIfSolved();
+        }
+
+        private void CheckIfSolved()
+        {
+            int buttonsUp = 0;
+
+            for (int i = 0; i < ovenButtons.Length; i++)
+            {
+                if(ovenButtons[i].IsSwitchUp() == true)
+                {
+                    buttonsUp++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if(buttonsUp == ovenButtons.Length)
+            {
+                GameManager.instance.ExitPuzzle(true);
+            }
+        }
+
+        private void MoveFinger()
+        {
+            if (!dpadActive && Mathf.Abs(Input.GetAxis("DPadHorizontal")) == 1)
+            {
+                dpadActive = true;
+
+                if (currentFinger == ovenFingers.Length)
+                {
+                    return;
+                }
+                else if (Input.GetAxis("DPadHorizontal") > 0 && currentFinger <= 2)
+                {
+                    UnsetButtonAndFinger();
+                    currentFinger++;
+                    SetButtonAndFinger();
+                }
+                else if (Input.GetAxis("DPadHorizontal") < 0 && currentFinger >= 1)
+                {
+                    UnsetButtonAndFinger();
+                    currentFinger--;
+                    SetButtonAndFinger();
+                }
+            }
+            else if (dpadActive && Mathf.Abs(Input.GetAxis("DPadHorizontal")) == 0)
+            {
+                dpadActive = false;
+            }
+
+        }
+
+        private void SetButtonAndFinger()
+        {
+            ovenFingers[currentFinger].SetActive(true);
+            currentOvenButton = ovenButtons[currentFinger];
+        }
+
+        private void UnsetButtonAndFinger()
+        {
+            currentOvenButton = null;
+            ovenFingers[currentFinger].SetActive(false);
+        }
+    }
+}
+
